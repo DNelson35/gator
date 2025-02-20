c053bedc175d528ebfcfe22d05353ea4c8212270
Damien Nelson <dhnisaman@gmail.com>
2025-02-20 01:00:42 -0500

    add register handler and update login

--- handler.go
+++ handler.go
@@ -1,18 +1,51 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"time"
+
+	"github.com/DNelson35/gator/internal/database"
+	"github.com/google/uuid"
 )
 
 
 func handlerLogin( s *state, cmd command) error {
 	if len(cmd.args) == 0 {
-		return fmt.Errorf("the login handler expects a single argument, the username")
+		return fmt.Errorf("login expects a username")
 	}
 
-	if err := s.pconfig.SetUser(cmd.args[0]); err != nil {
+	user, err := s.db.GetUser(context.Background(), cmd.args[0])
+	if err != nil {
+		return err
+	}
+
+	if err := s.pconfig.SetUser(user.Name); err != nil {
 		return err
 	}
 	fmt.Println("user set")
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return fmt.Errorf("register expects a username")
+	}
+	user, err := s.db.CreateUser(context.Background(), database.CreateUserParams{
+		ID: uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name: cmd.args[0],
+	})
+
+	if err != nil {
+		return err
+	}
+
+	if err = s.pconfig.SetUser(user.Name); err != nil {
+		return err
+	}
+
+	fmt.Printf("user created and loged in: %v", user)
 	return nil
 }
